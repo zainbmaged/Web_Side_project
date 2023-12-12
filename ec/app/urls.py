895658e0258809194b711aab925f16e324afeaed
Tasneem Mohamed <92854521+Tasneem849@users.noreply.github.com>
2023-12-12 19:05:18 +0200

    Update urls.py

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('register.html', views.register),
-    path("userprofile/", views.UserProfileView.view_as(), name="userprofile"),
+    path('UserProfile.html', views.userprofile),
 ]
