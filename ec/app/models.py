2ace43a6030b8aac3f7e24822fd3928995721138
Tasneem Mohamed <92854521+Tasneem849@users.noreply.github.com>
2023-12-12 18:43:24 +0200

    Update models.py

--- ec/app/models.py
+++ ec/app/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class instructor(models.Model):
+class Instructor(models.Model):
     Name = models.CharField(max_length=255)
     organization = models.CharField(max_length=255)
     numberofcourse = models.IntegerField()
     Field = models.CharField(max_length=255)
     rating = models.DecimalField(max_digits=2, decimal_places= 2)
+
+class User(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Profile_Picture = models.ImageField(upload_to='PPimages')
+    Email = 
+    Password = 
+    Name = 
